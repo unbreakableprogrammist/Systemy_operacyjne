fcfecc9b8574a1f9d6a8c02e6b4efcdca7632816
Krzychulec <criskbp.2019@gmail.com>
2025-12-02 12:28:12 +0100

    kocham legie1

--- zadania_2/zadanie2.c
+++ zadania_2/zadanie2.c
@@ -20,20 +20,43 @@ void usage(char *name) {
     exit(EXIT_FAILURE);
 }
 
-volatile sig_atomic_t parent_running = 1;
-volatile sig_atomic_t ill = 0;
-volatile sig_atomic_t should_be_ill = 0;
+volatile sig_atomic_t parent_running = 1; //czy nie minela sekunda 
+volatile sig_atomic_t ill = 0; // czy dziecko jest chore
+volatile sig_atomic_t should_be_ill = 0;  // czy moze zachorowac ( prawdopodobienstwo )
+volatile sig_atomic_t licznik = 0;  // licznik kaszlniec na mnie
 void alarm_handler(int sig){
     parent_running = 0;
 }
+void siguser1_handler(int sig){
+    if(should_be_ill && !ill)
+        ill=1;
+    licznik++;
+}
 
-void children_work(){
+void children_work(int p,int k){
+    srand(time(NULL)*getpid());
     printf("I have been created ! , My pid is : %d\n",getpid());
-    struct timespec ts = {0,100000000};
+    // handlowanie sigtermem
+    struct sigaction sa;
+    memset(&sa,0,sizeof(sa));
+    sa.sa_handler = alarm_handler; // robi to samo co alarm , oznacza ze rodzic skonczyl prace
+    if(sigaction(SIGTERM,&sa,NULL)<0)
+        ERR("sigaction");
+    // losujemy losowy czas i prawdopodobienstwo
+    int random_time = 50+ rand()%151;
+    struct timespec ts = {0,1000000*random_time};
+    // sprawdzamy czy wylosowane prawdopodobienstwo jest lepsze niz podane
+    int probability = 1+rand()&101;
+    if(probability>=p) should_be_ill = 1; // mowimy ze mozemy zachorowac
 
+    //handlowanie siguser1
+    memset(&sa,0,sizeof(sa));
+    sa.sa_handler = siguser1_handler; // jesli mozemy byc chorzy i ktos w nas strzeli to chorujemy
+    if(sigaction(SIGUSR1,&sa,NULL)<0)
+        ERR("sigaction");
     while(parent_running){
         nanosleep(&ts,NULL);
-        printf("Living la vida loca [PID]: %d\n",getpid());
+        
     }
 }
 
@@ -56,7 +79,7 @@ int main(int argc,char** argv){
         dziecko=fork();
         if(dziecko<0) ERR("fork");
         if(dziecko==0){
-            children_work();
+            children_work(p,k);
             exit(EXIT_SUCCESS);
         }      
     }
@@ -67,7 +90,9 @@ int main(int argc,char** argv){
         ERR("sigaction");
     alarm(t);
     while(parent_running){
+        // <- tutaj waitpid i jesli ktores dziecko wyjdzie to chore i zabrane przez rodzicow 
     }
+    // tutaj waitpid to po zakonczaniu programu wiec dzieci chore albo zdrowe ale w przedszkolu 
     if(kill(0,SIGTERM)<0) ERR("kill");
     exit(EXIT_SUCCESS);
 }
