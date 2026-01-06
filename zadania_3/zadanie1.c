ac26ca9a4cb71399de7e1bc0ca1911189be72ea7
krzysztof <krzysztof.wojczakowski@gmail.com>
2026-01-06 16:06:17 +0100

    zadania wlasne

---
+++ zadania_3/zadanie1.c
@@ -0,0 +1,18 @@
+#include <errno.h>
+#include <pthread.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+#define ERR(source) (perror(source), fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), exit(EXIT_FAILURE))
+
+int main(int argc, char **argv) {
+    if (argc != 2) ERR("Wrong number of arguments");
+
+    return EXIT_SUCCESS;
+}
