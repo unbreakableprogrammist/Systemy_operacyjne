1251a3095e07e3d7765f68e2b90489ec17159252
Krzychulec <criskbp.2019@gmail.com>
2026-01-07 13:30:35 +0100

    zadanie 3

--- zadania_3/zadanie3.c
+++ zadania_3/zadanie3.c
@@ -12,37 +12,41 @@
 
 #define ERR(source) (perror(source), fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), exit(EXIT_FAILURE))
 
+// Zmienna globalna do kontrolowania działania wątków
 volatile sig_atomic_t running = 1;
 
 typedef struct arg{
-    int* tor;
-    pthread_mutex_t* mutexy;
-    int n; 
-    int k; 
-    int first_place;
-    int second_place;
-    int third_place;
-    pthread_mutex_t first_place_mutex;
+    int* tor; // tor ( tablica ktora jest mallocowana w main )
+    pthread_mutex_t* mutexy;  // tablica mutexow do zabezpieczenia pozycji na torze
+    int n; // dlugosc toru
+    int k; // ilosc psow
+    // miejsca na podium i mutexy do ich zabezpieczenia
+    int first_place;   
+    int second_place;   
+    int third_place;  
+    pthread_mutex_t first_place_mutex;  
     pthread_mutex_t second_place_mutex;
     pthread_mutex_t third_place_mutex;
+    // licznik ile na mecie psow 
     int ile_na_mecie;
     pthread_mutex_t ile_na_mecie_mutex;
 } arg;
 
+// struktura przekazywana do wątku psa( ma unikalne id i wskaźnik do arg ( ktory jest ogolny dla wszystkich psów ) )
 typedef struct dog{
     int id;
     arg* arguments;
 } dog;
 
+// struktura przekazywana do wątku sygnałowego dostaje wskaźnik do tablicy wątków psów i ich ilość
 typedef struct signal_args{
     pthread_t* watki;
     int k;
 } signal_args;
 
-// Pusty handler jest OK, ale lepiej żeby nic nie robił, 
-// bo printf w handlerze cleanup to ryzyko.
+
 void przerwanie(void* arg){
-    // Opcjonalnie tutaj można by odblokować mutex, gdybyśmy wiedzieli który.
+
 }
 
 void* thread_work(void* argument) {
@@ -56,95 +60,103 @@ void* thread_work(void* argument) {
     unsigned int seed = time(NULL) ^ id;
     
     int position = 0; 
-    int direction = 1;
-
+    
+    // Wejście na start
+    int random_time = 200 + rand_r(&seed) % 1321;
+    struct timespec ts= {0, random_time * 1000000};
+    nanosleep(&ts, NULL);
+    
     pthread_mutex_lock(&mutexy[0]);
     tor[0] += 1;
     pthread_mutex_unlock(&mutexy[0]);
 
-    // POPRAWKA: Push przed pętlą
     pthread_cleanup_push(przerwanie, NULL);
-
+    
     while(running){
-        int random_time = 200 + rand_r(&seed) % 1321;
-        struct timespec ts= {0, random_time * 1000000};
+        random_time = 200 + rand_r(&seed) % 1321;
+        ts.tv_nsec = random_time * 1000000;
         nanosleep(&ts, NULL);
-
+        
         int skok = 1 + rand_r(&seed) % 5;
-        int next_pos = position + (skok * direction);
-
+        int next_pos = position + skok;
+        
+        // Logika odbicia
         if (next_pos >= n) {
-            next_pos = n - 1; 
-            direction = -1; 
+            next_pos = n - 1 - (next_pos - (n - 1));
         } 
-        else if (next_pos < 0) {
-            next_pos = 0;
-            direction = 1;
-        }
-
+        
+        // jesli pies zmienil pozycje 
         if (next_pos != position) {
             pthread_mutex_lock(&mutexy[next_pos]);
             tor[next_pos] += 1;
             bool tlok = (tor[next_pos] > 1);
             pthread_mutex_unlock(&mutexy[next_pos]);
-
+            
             if (tlok) {
                 printf("waf waf waf , pies: %d, na pozycji: %d\n", id, next_pos);
             }
-
-            pthread_mutex_lock(&mutexy[position]);
-            tor[position] -= 1;
-            pthread_mutex_unlock(&mutexy[position]);
-
-            position = next_pos;
-        }
-
-        if (position == n - 1) {
-            pthread_mutex_lock(&args->ile_na_mecie_mutex);
-            args->ile_na_mecie += 1;
-            pthread_mutex_unlock(&args->ile_na_mecie_mutex);
-
-            pthread_mutex_lock(&args->first_place_mutex);
-            if(args->first_place == -1){
-                args->first_place = id;
-                printf("Pies %d zajął 1 miejsce!\n", id);
-                pthread_mutex_unlock(&args->first_place_mutex);
-                goto finish; // Używamy goto żeby wyskoczyć do sprzątania
-            }
-            pthread_mutex_unlock(&args->first_place_mutex);
-
-            pthread_mutex_lock(&args->second_place_mutex);
-            if(args->second_place == -1){
-                args->second_place = id;
-                printf("Pies %d zajął 2 miejsce!\n", id);
-                pthread_mutex_unlock(&args->second_place_mutex);
-                goto finish;
-            }
-            pthread_mutex_unlock(&args->second_place_mutex);
-
-            pthread_mutex_lock(&args->third_place_mutex);
-            if(args->third_place == -1){
-                args->third_place = id;
-                printf("Pies %d zajął 3 miejsce!\n", id);
-                pthread_mutex_unlock(&args->third_place_mutex);
-                goto finish;
-            }
-            pthread_mutex_unlock(&args->third_place_mutex);
-
-            printf("Pies %d na mecie (poza podium)\n", id);
-
-            finish:
+            
             pthread_mutex_lock(&mutexy[position]);
             tor[position] -= 1;
             pthread_mutex_unlock(&mutexy[position]);
             
-            // Teraz break zadziała poprawnie, bo jesteśmy wewnątrz while, 
-            // ale push jest na zewnątrz!
-            break; 
+            position = next_pos;
+        }
+
+        // --- OBSŁUGA METY BEZ GOTO ---
+        if (position == n - 1) {
+            // Zwiększ licznik psów na mecie
+            pthread_mutex_lock(&args->ile_na_mecie_mutex);
+            args->ile_na_mecie += 1;
+            pthread_mutex_unlock(&args->ile_na_mecie_mutex);
+
+            bool czy_na_podium = false; // Flaga sterująca
+
+            // Sprawdź 1. miejsce
+            pthread_mutex_lock(&args->first_place_mutex);
+            if(args->first_place == -1){
+                args->first_place = id;
+                printf("Pies %d zajął 1 miejsce!\n", id);
+                czy_na_podium = true;
+            }
+            pthread_mutex_unlock(&args->first_place_mutex);
+
+            // Sprawdź 2. miejsce (tylko jeśli nie ma podium)
+            if (!czy_na_podium) {
+                pthread_mutex_lock(&args->second_place_mutex);
+                if(args->second_place == -1){
+                    args->second_place = id;
+                    printf("Pies %d zajął 2 miejsce!\n", id);
+                    czy_na_podium = true;
+                }
+                pthread_mutex_unlock(&args->second_place_mutex);
+            }
+
+            // Sprawdź 3. miejsce (tylko jeśli nadal nie ma podium)
+            if (!czy_na_podium) {
+                pthread_mutex_lock(&args->third_place_mutex);
+                if(args->third_place == -1){
+                    args->third_place = id;
+                    printf("Pies %d zajął 3 miejsce!\n", id);
+                    czy_na_podium = true;
+                }
+                pthread_mutex_unlock(&args->third_place_mutex);
+            }
+
+            // Jeśli po sprawdzeniu wszystkich miejsc nadal nie ma podium
+            if (!czy_na_podium) {
+                printf("Pies %d na mecie (poza podium)\n", id);
+            }
+
+            // Wspólny kod sprzątający (dawniej pod etykietą finish:)
+            pthread_mutex_lock(&mutexy[position]);
+            tor[position] -= 1;
+            pthread_mutex_unlock(&mutexy[position]);
+            
+            break; // Wyjście z pętli while
         }
     }
 
-    // POPRAWKA: Pop po pętli
     pthread_cleanup_pop(0);
     return NULL;
 }
