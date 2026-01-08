1921a29f932922cab92e845cc556920e59957138
krzysztof <krzysztof.wojczakowski@gmail.com>
2026-01-08 18:28:11 +0100

    czesc zadania 2

--- zadania_3/zadanie_2/sop-vp.c
+++ zadania_3/zadanie_2/sop-vp.c
@@ -28,10 +28,27 @@ circular_buffer* circular_buffer_create() {
 }   
 
 // ta funckja musi puszowac wartosci , wazne jest to ze musi to robic TYLKO gdy n< BUFFERSIZE i musi robic to cyklicznie (%BUFFERSIZE)
-void circular_buffer_push(circular_buffer* buffer, video_frame* frame) {
+void circular_buffer_push(circular_buffer* cb, video_frame* frame) {
     int czy_udalo_sie=0;
+    struct timespec ts = {0,5000000}; // czas czekania to 5milisekund 
     while(!czy_udalo_sie){ // dopoki sie nie udaje
+        pthread_mutex_lock(&cb->n_mtx);
+        int n = cb->n;
+        pthread_mutex_unlock(&cb->n_mtx);
+        if(n >= BUFFER_SIZE) nanosleep(&ts,NULL); // jesli bufor jest pelen to busy waiting 5 ms
+        else{
+            pthread_mutex_lock(&cb->buffer_mtx);
+            cb->buffer[cb->head] = frame;
+            pthread_mutex_unlock(&cb->buffer_mtx);
+            pthread_mutex_lock(&cb->head_mtx);
+            cb->head=(cb->head+1)%BUFFER_SIZE; // zmieniamy heada i zakrecamy
+            pthread_mutex_unlock(cb->head_mtx);
+            n+=1;
+            pthread_mutex_lock(&cb->n_mtx);
+            cb->n = n;
+            pthread_mutex_unlock(&cb->n_mtx);
         
+        }
     }
 }
 // video_frame* circular_buffer_pop(circular_buffer* buffer) {}
